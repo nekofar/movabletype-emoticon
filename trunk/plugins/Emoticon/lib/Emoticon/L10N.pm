faac20950ce066cbdce910bd0d63d408a7c8ec0c
nekofar <nekofar@3bb12dda-a519-0410-847c-0bc294ee2c32>
2006-08-15 21:01:12 +0000

    New Import

---
+++ trunk/plugins/Emoticon/lib/Emoticon/L10N.pm
@@ -0,0 +1,11 @@
+# Emoticon plugin for Movable Type
+# Author: Milad Nekofar (http://www.nekofar.com)
+# Released under the Artistic License
+#
+# $Id$
+
+package Emoticon::L10N;
+use strict;
+use base 'MT::Plugin::L10N';
+
+1;
